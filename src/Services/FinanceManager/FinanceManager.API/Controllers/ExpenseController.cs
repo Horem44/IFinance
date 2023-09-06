5ecfe5cf9a928f9530abec40c5daa5fb189ebd66
Ilya Yegorov <dlyhartstouna@gmail.com>
2023-09-06 23:36:55 +0300

    feat (finance manager): controllers

---
+++ src/Services/FinanceManager/FinanceManager.API/Controllers/ExpenseController.cs
@@ -0,0 +1,28 @@
+﻿using FinanceManager.API.Application.Commands.Expenses.AddExpenseCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExpenseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddExpense(
+            AddExpenseCommand command,
+            CancellationToken cancellationToken
+        )
+        {
+            var expenseId = await _mediator.Send(command, cancellationToken);
+            return Ok(expenseId);
+        }
+    }
+}
---
+++ src/Services/FinanceManager/FinanceManager.API/Controllers/RevenueController.cs
@@ -0,0 +1,28 @@
+﻿using FinanceManager.API.Application.Commands.Revenues.AddRevenueCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RevenueController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RevenueController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRevenue(
+            AddRevenueCommand command,
+            CancellationToken cancellationToken
+        )
+        {
+            var revenueId = await _mediator.Send(command, cancellationToken);
+            return Ok(revenueId);
+        }
+    }
+}
